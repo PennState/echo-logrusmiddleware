1d01bf12bc1953afb03f9713a1ca590f7ce32f57
Nathan Bowler <nzb301@psu.edu>
2019-06-13 08:03:40 -0400

    Move bridge to its own file for clarity. Fix issue where some bridge methods use the logrus StandardLogger and others used the embedded logger.

--- middleware.go
+++ middleware.go
@@ -1,102 +1,12 @@
 package logrusmiddleware
 
 import (
-	"io"
 	"strconv"
 	"time"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/gommon/log"
 	"github.com/sirupsen/logrus"
 )
-
-type Logger struct {
-	*logrus.Logger
-}
-
-func (l Logger) Level() log.Lvl {
-	switch l.Logger.Level {
-	case logrus.DebugLevel:
-		return log.DEBUG
-	case logrus.WarnLevel:
-		return log.WARN
-	case logrus.ErrorLevel:
-		return log.ERROR
-	case logrus.InfoLevel:
-		return log.INFO
-	default:
-		l.Panic("Invalid level")
-	}
-
-	return log.OFF
-}
-
-func (l Logger) SetPrefix(s string) {
-	// TODO
-}
-
-func (l Logger) Prefix() string {
-	// TODO.  Is this even valid?  I'm not sure it can be translated since
-	// logrus uses a Formatter interface.  Which seems to me to probably be
-	// a better way to do it.
-	return ""
-}
-
-func (l Logger) SetLevel(lvl log.Lvl) {
-	switch lvl {
-	case log.DEBUG:
-		logrus.SetLevel(logrus.DebugLevel)
-	case log.WARN:
-		logrus.SetLevel(logrus.WarnLevel)
-	case log.ERROR:
-		logrus.SetLevel(logrus.ErrorLevel)
-	case log.INFO:
-		logrus.SetLevel(logrus.InfoLevel)
-	default:
-		l.Panic("Invalid level")
-	}
-}
-
-func (l Logger) Output() io.Writer {
-	return l.Out
-}
-
-func (l Logger) SetOutput(w io.Writer) {
-	logrus.SetOutput(w)
-}
-
-func (l Logger) Printj(j log.JSON) {
-	logrus.WithFields(logrus.Fields(j)).Print()
-}
-
-func (l Logger) Debugj(j log.JSON) {
-	logrus.WithFields(logrus.Fields(j)).Debug()
-}
-
-func (l Logger) Infoj(j log.JSON) {
-	logrus.WithFields(logrus.Fields(j)).Info()
-}
-
-func (l Logger) Warnj(j log.JSON) {
-	logrus.WithFields(logrus.Fields(j)).Warn()
-}
-
-func (l Logger) Errorj(j log.JSON) {
-	logrus.WithFields(logrus.Fields(j)).Error()
-}
-
-func (l Logger) Fatalj(j log.JSON) {
-	logrus.WithFields(logrus.Fields(j)).Fatal()
-}
-
-func (l Logger) Panicj(j log.JSON) {
-	logrus.WithFields(logrus.Fields(j)).Panic()
-}
-
-func (l Logger) SetHeader(header string) {
-	// TODO:
-	logrus.Debug("SetHeader called with %s", header)
-}
 
 func logrusMiddlewareHandler(c echo.Context, next echo.HandlerFunc) error {
 	req := c.Request()
